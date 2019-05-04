cf3ae97a7045f93822bfc2630f3ff7638fb5ae20
August Johnson <gusjohnson98@gmail.com>
2019-05-04 12:40:18 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import LandingPage from "./pages";
+import Dashboard from "./pages";
+import CreateSession from "./pages";
+import FindSession from "./pages";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <h1>Jamster</h1>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/createSession" component={CreateSession} />
+        <Route exact path="/findSession" component={FindSession} />
+      </Switch>
+    </Router>
   );
 }
 
