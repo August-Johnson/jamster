3d645a950e4686a5b18515a095c5d5a904b0a972
August Johnson <gusjohnson98@gmail.com>
2019-05-06 17:00:06 -0500

    modules ignored

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+const routes = require("./routes");
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+app.use(routes);
+
+app.listen(PORT, () => {
+    console.log(`==> API Server listening on PORT ${PORT}!`);
+});
