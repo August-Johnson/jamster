b3f99bfdecbec5706a54569df1ff976f4f2e60f0
August Johnson <gusjohnson98@gmail.com>
2019-05-06 20:07:22 -0500

    no error

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,17 @@
 import React from 'react';
 //import logo from './logo.svg';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-//import { CreateSession, FindSession, Dashboard, LandingPage } from "./pages";
+import {BrowserRouter, Route} from "react-router-dom";
 import UserInfo from "./pages/userInfo/index";
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <h1>Jamster</h1>
-        <Route exact path="/userInfo" component={UserInfo} />
-        {/* <Route exact path="/" component={LandingPage} />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/createSession" component={CreateSession} />
-        <Route exact path="/findSession" component={FindSession} /> */}
-      </Switch>
-    </Router>
+    <div>
+      <h1>Jamster</h1><br />
+
+      <UserInfo />
+
+    </div>
   );
 }
 
