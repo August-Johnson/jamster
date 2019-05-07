700c5b05384b39c5e9a2b48e499ac632e0ee464c
August Johnson <gusjohnson98@gmail.com>
2019-05-06 20:15:39 -0500

    react router working

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 //import logo from './logo.svg';
-import {BrowserRouter, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import UserInfo from "./pages/userInfo/index";
 import './App.css';
 
 function App() {
   return (
     <div>
-      <h1>Jamster</h1><br />
-
-      <UserInfo />
-
+      <h1>Jamster</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/user" component={UserInfo} />
+        </Switch>
+      </Router>
     </div>
   );
 }
