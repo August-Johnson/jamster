94e2879761e03b5fdc019ca5fc5de5022f413994
August Johnson <gusjohnson98@gmail.com>
2019-05-07 12:06:12 -0500

    profile page skeleton (rough)

--- client/src/pages/myProfile/index.js
+++ client/src/pages/myProfile/index.js
@@ -1,18 +1,50 @@
 import React, { Component } from "react";
-import { Container } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Jumbotron from "react-bootstrap/Jumbotron";
 
 class UserInfo extends Component {
     state = {
         email: "someones-email@gmail.com",
-        instrument: "",
-        skillLevel: "",
+        instrument: "Guitar",
+        skillLevel: 3,
         username: "Gus"
     }
 
     render() {
         return (
             <Container>
-                <h1>Welcome to your page {this.state.username}</h1>
+
+
+                <Jumbotron>
+                    <Row>
+                        <Col>
+                            <h1 className="text-center mb-4">My Profile</h1>
+                            <hr />
+                            <br />
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col xl={12} lg={12} md={12} sm={12} xs={12}>
+                            <h2>Info:</h2>
+                            <br />
+                        </Col>
+
+                        <Col xl={5} lg={5} md={6} sm={12} xs={12}>
+                            <img src="https://via.placeholder.com/300x300" alt="null" />
+                        </Col>
+
+                        <Col xl={7} lg={7} md={6} sm={12} xs={12}>
+                            <h3>Username: {this.state.username} </h3>
+                            <h3>Email: {this.state.email} </h3>
+                            <h3>instrument: {this.state.instrument} </h3>
+                            <h3>Skill Level: {this.state.skillLevel} </h3>
+                        </Col>
+                    </Row>
+                </Jumbotron>
+
             </Container>
         );
     }
