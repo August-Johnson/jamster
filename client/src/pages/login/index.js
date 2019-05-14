a401339645b761aced368e11333039e8a75ed079
Jasper Beachy <jbeachy@gustavus.edu>
2019-05-13 20:29:28 -0500

    login fixed

--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -3,7 +3,8 @@ import Hero from "../../components/Hero";
 import Container from "../../components/Container";
 import Background from "./music_background.jpg";
 import Wrapper from "../../components/Wrapper";
-import {Button, InputGroup, FormGroup, FormControl} from "react-bootstrap";
+
+import {Button, InputGroup, Image, FormGroup, FormControl, Row, Col} from "react-bootstrap";
 
 class login extends Component {
   state = {
@@ -16,27 +17,33 @@ class login extends Component {
     this.setState({
       [name]:value
     }) 
-    
   }
 
-  
+
   render() {
     return (
-      <Wrapper>
-      <Hero backgroundImage={Background}></Hero>
+      
+      
+      
       <Container>
+      
+      <Hero backgroundImage={Background}></Hero>
       <br></br>
+      <Row className="justify-content-md-center"> 
+        <Col lg="4">
       <InputGroup><FormControl placeholder="Username"aria-label="Username"aria-describedby="basic-addon1" name="username" value={this.state.username} onChange={this.onChange}/></InputGroup>
       <br></br>
       <InputGroup><FormControl placeholder="Password"aria-label="Password"aria-describedby="basic-addon1" name="password" value={this.state.password} onChange={this.onChange}/></InputGroup>
 
       <br></br>
-      <Button type="submit" variant="outline-primary">Submit</Button>
-      <Button type="submit" variant="outline-info"><a href="/createAccount">Create Account</a></Button>
-
+      <Button className="mb-3" type="submit" variant="outline-primary">Submit</Button>
+      <Button className="mb-3" type="submit" variant="outline-info"><a href="/createAccount">Create Account</a></Button>
+      </Col>
+      </Row>
+      
       </Container>
         
-      </Wrapper>
+      
     );
   }
 }
