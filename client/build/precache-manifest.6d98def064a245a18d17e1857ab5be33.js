49e885e4ebb42623a79e44b0dd7b24632cdfe633
August Johnson <gusjohnson98@gmail.com>
2019-06-05 20:46:22 -0500

    build

---
+++ client/build/precache-manifest.6d98def064a245a18d17e1857ab5be33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4ef37d779ef989d37a98851bed63eb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1e42152d116fd780056",
+    "url": "/static/css/main.98fa35a3.chunk.css"
+  },
+  {
+    "revision": "12c2f1657ddc529330b5",
+    "url": "/static/js/2.dc699db8.chunk.js"
+  },
+  {
+    "revision": "a1e42152d116fd780056",
+    "url": "/static/js/main.cf46bbe4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "67d11882b3a03509de18e8275da0dc2f",
+    "url": "/static/media/wallpaper2.67d11882.jpg"
+  }
+]);
